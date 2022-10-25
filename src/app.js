df26c7eda3138cdd69d9d0e62dee79143afdfc1b
Graciele Sampaio <graciele-1996@hotmail.com>
2022-10-25 11:54:02 -0300

    requisito 12 de novo kkk

--- src/app.js
+++ src/app.js
@@ -14,10 +14,9 @@ app.use('/sales', salesRouter);
 app.get('/', (_request, response) => {
   response.send();
 });
+// Agradecimentos a João Gustavo, Guilherme Palma, Rogério Lins, Edmilson, Lalá e Carla Uyemura 
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
 module.exports = app;
-
-// Agradecimentos a João Gustavo, Guilherme Palma, Rogério Lins, Edmilson, Lalá e Carla Uyemura 
